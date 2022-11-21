c57a8c230c37cda9d95f8d369d3148a6ce80ba32
edwar4rd <edwar4rd@gmail.com>
2022-11-22 00:23:30 +0800

    test commit: finish program v0.0.1

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    loop {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        if buffer == "q\n" {
+            break;
+        }
+        println!("v0.0.1");
+    }
+    Ok(())
+}
